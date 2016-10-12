10ef17a49865d1b2bd1078dd9af0118853a473d8
lazuruz <austinmshea@gmail.com>
2016-10-12 13:51:22 -0700

    Adding commands

--- blow_away_keychain.sh
+++ blow_away_keychain.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 #designed for and tested on Mac OS X 10.8 - 10.12
 
-echo "cd ~/Library/Keychains"
+cd ~/Library/Keychains
 
-print "We have arrived" 
+echo "we are here: "
+
+echo pwd
+
+echo "We have arrived" 
 
 echo "The computer needs to reboot.  Is this ok?  type "y" for yes or "n" for no followed by enter/return."
 
