7c85273f8ee0eff555e46650eb89bdeb45649fd9
lazuruz <austinmshea@gmail.com>
2016-10-12 13:25:03 -0700

    Removing damage done by RTF formatting

--- blow_away_keychain.sh
+++ blow_away_keychain.sh
@@ -1,12 +1,9 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1348\cocoasubrtf170
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset0 AndaleMono;}
-{\colortbl;\red255\green255\blue255;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural
+#!/bin/bash
+#designed for and tested on Mac OS X 10.8 - 10.12
 
-\f0\fs24 \cf0 #!/bin/bash\
-\
-sudo rm -rf ~/
-\f1 \CocoaLigature0 Library/Keychains/*\
-echo \'93All complete.  Computer will now restart\'94\
-sudo reboot}
+echo "cd ~/Library/Keychains"
+
+print "We have arrived" 
+
+echo "The computer needs to reboot.  Is this ok?  type "y" for yes or "n" for no followed by enter/return."
+
