32630b1180c18bd28c89dee64a7a963a29dafa52
lazuruz <austinmshea@gmail.com>
2016-10-14 15:34:12 -0700

    Corrected some bad commands.  This is not production yet.

--- blow_away_keychain.sh
+++ blow_away_keychain.sh
@@ -10,9 +10,10 @@ echo "This program is removing Keychain libraries located at: "
 'pwd'
 
 #removes user keychains from login point
-rm -rf ~/Library/Keychains/
+rm -rf ~/Library/Keychains/*
 
 #asks user if its ok to reboot
 echo "The computer needs to reboot.  Is this ok?  type "y" for yes or "n" for no followed by enter/return."
 
-
+#restarts - *nix stuff needs elevated perms
+'sudo reboot'
