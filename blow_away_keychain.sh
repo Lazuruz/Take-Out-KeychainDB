e5aa60fba27a2a9756da44f70ac0ca5f5979924d
Lazuruz <austinmshea@gmail.com>
2018-02-09 15:54:28 -0800

    Changed so it actually asks user

--- blow_away_keychain.sh
+++ blow_away_keychain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #designed for and tested on Mac OS X 10.8 - 10.12
 
-cd ~/Library/Keychains
+#cd ~/Library/Keychains
 
 #Tells user about location
 echo "This program is removing Keychain libraries located at: "
@@ -10,10 +10,14 @@ echo "This program is removing Keychain libraries located at: "
 'pwd'
 
 #removes user keychains from login point
-rm -rf ~/Library/Keychains/*
+#rm -rf ~/Library/Keychains/*
 
-#asks user if its ok to reboot
-echo "The computer needs to reboot.  Is this ok?  type "y" for yes or "n" for no followed by enter/return."
-
-#restarts - *nix stuff needs elevated perms
-'sudo reboot'
+#asks user if its ok to reboot - thanks to StackOverflow ... im a noob
+while true; do
+	read -p "The computer needs to reboot.  Is this ok?  type "y" for yes or "n" for no followed by enter/return." yn
+	case $yn in
+		[Yy]* ) sudo reboot; break;;  #reboot's user computer for changes
+		[Nn]* ) exit;;
+		* ) echo "Please answer 'y' or 'n'";;
+	esac
+done
